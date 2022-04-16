92890562c660715660fb8e9b85b3b8659481ba3a
vihaanpawar <103321047+vihaanpawar@users.noreply.github.com>
2022-04-16 19:41:29 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,8 @@ var seaImg,shipImg;
 function preload(){
   //uncomment the code to add animation to ship 
 
-  shipImg = loadAnimation("ship-1.png");
-  shipImg = loadAnimation("ship-1.png");
-  shipImg = loadAnimation("ship-1");
+  
   shipImg = loadAnimation("ship-1.png","ship-2.png","ship-1.png","ship-2.png");
-  shipImg = loadAnimation("ship-1","ship-2","ship-1","ship-2");
   
   seaImg = loadImage("sea.png");
 }
@@ -25,7 +22,7 @@ function setup(){
 
   
   ship = createSprite(130,200,30,30);
-  ship.addAnimation("movingShip",shipImg1);
+  ship.addAnimation("movingShip",shipImg);
   ship.scale =0.25;
   
 }
